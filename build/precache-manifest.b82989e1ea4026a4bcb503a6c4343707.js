977223bf58df9f7a36f471651edb6ffaceaa5dad
Vishal Mangla <76559165+vishal-0070@users.noreply.github.com>
2022-11-12 10:44:43 +0530

    Add files via upload
    build update

---
+++ build/precache-manifest.b82989e1ea4026a4bcb503a6c4343707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc9df99d8ff5c7afb4ab",
+    "url": "/static/css/main.812e701d.chunk.css"
+  },
+  {
+    "revision": "bc9df99d8ff5c7afb4ab",
+    "url": "/static/js/main.bc9df99d.chunk.js"
+  },
+  {
+    "revision": "9a706a69ef0a8c3185a3",
+    "url": "/static/js/1.9a706a69.chunk.js"
+  },
+  {
+    "revision": "7ba1180137c47314e908",
+    "url": "/static/js/2.7ba11801.chunk.js"
+  },
+  {
+    "revision": "ef923b0091c2a93bc9f2",
+    "url": "/static/js/runtime~main.ef923b00.js"
+  },
+  {
+    "revision": "3e68af0f38040f691b2492c5cdaae322",
+    "url": "/index.html"
+  }
+];
